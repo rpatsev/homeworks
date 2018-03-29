ca041a5842d81df8e8613b5b93340046aeee1fd1
rpatsev <rpatsev@gmail.com>
2018-03-29 16:18:52 +0300

    Regexp task uploaded

---
+++ RegExpTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace RegExpTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"A regular expression, regex or regexp[1] (sometimes called a rational expression)[2][3] is, in theoretical computer science and formal language theory, 
+                            a sequence of characters that define a search pattern. Usually this pattern is then used by string searching algorithms for ""find"" or ""find and replace"" 
+                            operations on strings.
+
+                            The concept arose in the 1950s when the American mathematician Stephen Cole Kleene formalized the description of a regular language.
+                            The concept came into common use with Unix text - processing utilities.Since the 1980s, different syntaxes for writing regular expressions exist, 
+                            one being the POSIX standard and another, widely used, being the Perl syntax.
+
+                            Regular expressions are used in search engines, search and replace dialogs of word processors and text editors, in text processing utilities 
+                            such as sed and AWK and in lexical analysis.Many programming languages provide regex capabilities, built -in or via libraries.";
+
+
+            string patternForLinks = @"(\[(\d+)\])";
+
+            string correctedText = Regex.Replace(text, patternForLinks, String.Empty);
+            Console.WriteLine(@"Corrected text: {0}" ,correctedText);
+
+            
+            Regex patternForYears = new Regex(@"\d{4}[s]");
+            MatchCollection years = patternForYears.Matches(text);
+
+            foreach(Match match in years)
+            {
+                Console.WriteLine(match);
+            }
+
+            Regex patternForWordRegular = new Regex(@"\bregular\b", RegexOptions.IgnoreCase);
+            MatchCollection wordsRegular = patternForWordRegular.Matches(text);
+            //int timesRepeat = wordsRegular.Count;
+            Console.WriteLine(@"Regular word is repeated {0} times", wordsRegular.Count);
+        }        
+    }
+}
